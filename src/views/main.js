d382ea3575913f558b32eb7779bb0713999c28b6
Yelly Putri Wahyu Ningsih <yellyputriw@gmail.com>
2022-03-30 18:59:47 +0700

    :iphone: Add reponsif navbar

---
+++ src/views/main.js
@@ -0,0 +1,11 @@
+// Components
+import '@components/Navbar';
+import '@components/Hero';
+import '@components/Story';
+import '@components/Menu';
+import '@components/Location';
+import '@components/Contact';
+import '@components/Footer';
+
+// Utils
+import '@utils/script.js';
